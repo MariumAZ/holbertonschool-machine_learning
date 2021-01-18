7502d7100f1c02033dc17fc7f3bf7ff1aeaf9875
MariumAZ <mariem.azouz@dauphine.tn>
2021-01-18 14:24:31 +0100

    confusion matrix

---
+++ supervised_learning/0-create_confusion.py
@@ -0,0 +1,12 @@
+def create_confusion_matrix(labels,logits):
+    """
+    creates a confusion matrix.
+
+    """
+    m,classes = labels.shape
+    matrix = np.zeros(shape=(classes,classes))
+    for i in range(m):
+       a = np.where(labels[i,:]==1)
+       b = np.where(logits[i,:]==1)
+       matrix[a,b]+=1
+    return matrix   
