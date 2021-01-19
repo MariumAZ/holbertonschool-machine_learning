1fc946510e564f4c721e6f2ea63272655781a928
MariumAZ <mariem.azouz@dauphine.tn>
2021-01-19 15:36:37 +0100

    test

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -10,4 +10,4 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     Sum = 0
     for i in range(1, L+1):
         Sum += np.linalg.norm(weights["W"+str(i)])
-    return cost + (lambtha/ (2 * m)) * Sum
+    return cost + (lambtha / (2 * m)) * Sum
