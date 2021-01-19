4192977bfee650827ce857e5fe660d627644ed5b
MariumAZ <mariem.azouz@dauphine.tn>
2021-01-19 12:22:30 +0100

    test L2.

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-
+"""L2 regularization """
 import numpy as np
+
 def l2_reg_cost(cost, lambtha, weights, L, m):
 
     """ function that calculates the cost of a neural
@@ -9,4 +10,4 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     Sum = 0
     for i in range(1, L+1):
         Sum += np.linalg.norm(weights["W"+str(i)])
-    return cost+ (lambtha/ (2 * m)) * Sum
+    return cost + (lambtha/ (2 * m)) * Sum
