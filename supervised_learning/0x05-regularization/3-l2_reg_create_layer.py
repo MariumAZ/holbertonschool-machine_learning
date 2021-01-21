407189cc092be3d53f0173b846ea04d734a5691a
MariumAZ <mariem.azouz@dauphine.tn>
2021-01-21 12:28:06 +0100

    test

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -19,5 +19,5 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
                                    kernel_initializer=init,
                                    kernel_regularizer=kernel)
     return(linear_model(prev))
-    
+
 
