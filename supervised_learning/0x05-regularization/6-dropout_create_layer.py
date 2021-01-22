b8fcda3078cfad9258d83a9332bc819dd1289547
MariumAZ <mariem.azouz@dauphine.tn>
2021-01-22 09:29:21 +0100

    test6

--- supervised_learning/0x05-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -3,10 +3,11 @@
 
 import tensorflow as tf
 
+
 def dropout_create_layer(prev, n, activation, keep_prob):
     """ function that creates a layer of a neural network
         using dropout"""
-    #dr = tf.layers.Dropout(keep_prob)
+   
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     linear_model = tf.layers.Dense(units=n, activation=activation,
                                    kernel_initializer=init)(prev)
