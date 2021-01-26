7f062f18319d83a27aaf95037e6cb2a40ef129ab
MariumAZ <mariem.azouz@dauphine.tn>
2021-01-26 11:43:08 +0100

    test

---
+++ supervised_learning/0x06-keras/4-train.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" train model """
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=False, shuffle=False):
+    """ Function that trains a model using mini-batch
+        gradient descent """
+    history = network.fit(x=data, y=labels, epochs=epochs, verbose=verbose,
+                          batch_size=batch_size, shuffle=shuffle)
+    return history
