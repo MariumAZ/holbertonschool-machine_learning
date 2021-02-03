3a224598548da57395f3f0635a20c944c3fd3d62
MariumAZ <mariem.azouz@dauphine.tn>
2021-02-03 16:24:14 +0100

    cnn pooling backward

---
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""  """
+import numpy as np
+
+def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
+  m,h_new,w_new,c_new = dA.shape
+  dA_prev = np.zeros(A_prev.shape)
+  kh, kw = kernel_shape[0], kernel_shape[1]
+  sh, sw = stride[0], stride[1]
+  
+  for i in range(m):
+    for h in range(h_new):
+        for w in range(w_new):
+          for c in range(c_new):
+              A_prev_mini = A_prev[i, h*sh:h*sh+kh, w*sw:w*sw+kw, c]
+              mask = (A_prev_mini == np.max(A_prev_mini))
+              dA_prev[i, h*sh:h*sh+kh, w*sw:w*sw+kw, c] += mask*dA[i, h, w, c]
+  return dA_prev    
