f8da5c883916d2eb66ca6ade08c1375f5bc99bc4
MariumAZ <mariem.azouz@dauphine.tn>
2021-02-05 09:07:33 +0100

    test

--- supervised_learning/0x07-cnn/3-pool_backward.py
+++ supervised_learning/0x07-cnn/3-pool_backward.py
@@ -7,8 +7,7 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
   dA_prev = np.zeros(A_prev.shape)
   kh, kw = kernel_shape[0], kernel_shape[1]
   sh, sw = stride[0], stride[1]
-  
-  #average = dz / (n_H * n_W)
+ 
   for i in range(m):
      for h in range(h_new):
         for w in range(w_new):
@@ -16,7 +15,7 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
               A_prev_mini = A_prev[i,h*sh:h*sh+kh,w*sw:w*sw+kw,c]
               if mode == "max":
                          mask = (A_prev_mini == np.max(A_prev_mini))
-              elif mode == "average":
+              else:
                          average = 1 / (kh * kw )
                          mask = average * np.ones(kernel_shape)
               dA_prev[i,h*sh:h*sh+kh,w*sw:w*sw+kw,c] += mask * dA[i,h,w,c]
