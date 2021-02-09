29843275afd3c2950e0c52b95b6b96efe6006cd2
MariumAZ <mariem.azouz@dauphine.tn>
2021-02-09 11:07:03 +0100

    inception_block

---
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import tensorflow.keras as K
+
+def identity_block(A_prev, filters): 
+    F11, F3, F12 = filters
+    layer = K.layers.Conv2D(F11,(1,1),strides=1,padding='same')(A_prev)
+    layer = K.layers.BatchNormalization(axis=3)(layer)
+    layer = K.layers.Activation(activation='relu')(layer)
+    #second layer 
+    layer = K.layers.Conv2D(F3,(3,3),strides=1,padding='same')(layer)
+    layer = K.layers.BatchNormalization(axis=3)(layer)
+    layer = K.layers.Activation(activation='relu')(layer)
+    #third layer
+    layer = K.layers.Conv2D(F12,(1,1),strides=1,padding='same')(layer)
+    layer = K.layers.BatchNormalization(axis=3)(layer)
+    layer = K.layers.Add()([layer, A_prev])
+    layer = K.layers.Activation(activation='relu')(layer)
+    return layer
+
+
+
+
+
+    
