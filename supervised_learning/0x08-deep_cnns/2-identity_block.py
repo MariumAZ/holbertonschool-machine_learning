24f92b883837bf0a96f91543965d18fdc7cc03fc
MariumAZ <mariem.azouz@dauphine.tn>
2021-02-09 11:11:41 +0100

    inception_block

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -4,15 +4,15 @@ import tensorflow.keras as K
 def identity_block(A_prev, filters):
     init = K.initializers.he_normal()
     F11, F3, F12 = filters
-    layer = K.layers.Conv2D(F11,(1,1),strides=1,padding='same',kernel_inizializer=init)(A_prev)
+    layer = K.layers.Conv2D(F11,(1,1),strides=1,padding='same',kernel_initializer=init)(A_prev)
     layer = K.layers.BatchNormalization(axis=3)(layer)
     layer = K.layers.Activation(activation='relu')(layer)
     #second layer 
-    layer = K.layers.Conv2D(F3,(3,3),strides=1,padding='same',kernel_inizializer=init)(layer)
+    layer = K.layers.Conv2D(F3,(3,3),strides=1,padding='same',kernel_initializer=init)(layer)
     layer = K.layers.BatchNormalization(axis=3)(layer)
     layer = K.layers.Activation(activation='relu')(layer)
     #third layer
-    layer = K.layers.Conv2D(F12,(1,1),strides=1,padding='same',kernel_inizializer=init)(layer)
+    layer = K.layers.Conv2D(F12,(1,1),strides=1,padding='same',kernel_initializer=init)(layer)
     layer = K.layers.BatchNormalization(axis=3)(layer)
     layer = K.layers.Add()([layer, A_prev])
     layer = K.layers.Activation(activation='relu')(layer)
