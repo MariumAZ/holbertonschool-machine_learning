8677776dda37e9bb31e949a6b32bf03301200666
MariumAZ <mariem.azouz@dauphine.tn>
2021-02-09 10:45:50 +0100

    inception

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -3,6 +3,7 @@ import tensorflow.keras as K
 
 def inception_block(A_prev, filters):
     
+    he_init = K.initializers.he_normal()
     F1, F3R, F3, F5R, F5, FPP = filters
     layer_1 = K.layers.Conv2D(F1,(1,1),padding='same',activation='relu')(A_prev)
     layer_2 = K.layers.Conv2D(F3R,(1,1),padding='same',activation='relu')(A_prev)
