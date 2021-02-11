4a2554b9b1a8e3a0cfd5d8ccaec0c2cc12712e04
MariumAZ <mariem.azouz@dauphine.tn>
2021-02-11 14:52:25 +0100

    transfer learning

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 
-import tensorflow.keras as k
+import tensorflow.keras as K
 def preprocess_data(X,Y):
-  X = k.applications.densenet.preprocess_input(X)
-  Y = k.utils.to_categorical(Y, 10)
+  X = K.applications.densenet.preprocess_input(X)
+  Y = K.utils.to_categorical(Y, 10)
   return X,Y
 
-input1 = k.layers.Input(shape=(32,32,3))
-input = k.layers.Lambda(lambda image: tf.image.resize(image,(224, 224)))(input1)
-dense = k.applications.DenseNet121(include_top=False,weights='imagenet',input_tensor=input)
+input1 = K.layers.Input(shape=(32,32,3))
+input = K.layers.Lambda(lambda image: tf.image.resize(image,(224, 224)))(input1)
+dense = K.applications.DenseNet121(include_top=False,weights='imagenet',input_tensor=input)
 output = dense.output
 #x = k.layers.Lambda(lambda i:K.resize_images(i,224,224,data_format="channels_last"))(output)
-x = k.layers.GlobalAveragePooling2D()(output)
-x = k.layers.Dense(512,activation='relu')(x)
-x = k.layers.Dense(10,activation='softmax')(x)
-Model = k.models.Model(inputs=input1,outputs=x)
-Model.summary()
-Model.compile(loss='categorical_crossentropy',optimizer=k.optimizers.RMSprop(lr=2e-5),metrics=['accuracy'])
+x = K.layers.GlobalAveragePooling2D()(output)
+x = K.layers.Dense(512,activation='relu')(x)
+x = K.layers.Dense(10,activation='softmax')(x)
+Model = K.models.Model(inputs=input1,outputs=x)
+Model.compile(loss='categorical_crossentropy',optimizer=K.optimizers.RMSprop(lr=2e-5),metrics=['accuracy'])
 for layer in Model.layers[:-3]:
   layer.trainable = False
 for layer in Model.layers[-3:]:
   layer.trainable = True  
-Model.fit(x_train,y_train,epochs=5,batch_size=32, verbose=1) 
+check = K.callbacks.ModelCheckpoint(filepath="cifar10.h5",
+                                        monitor="val_acc",
+                                        mode="max",
+                                        save_best_only=True)  
+Model.fit(x_train,y_train,validation_data=(x_test,y_test),epochs=5,batch_size=32, verbose=1,callbacks=[check]) 
+
 
