95fbd00dc85570bb9c51dd05087cbfd5ea654321
MariumAZ <mariem.azouz@dauphine.tn>
2021-02-22 14:32:51 +0100

    test

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -29,41 +29,47 @@ class Yolo():
         boxes = []
         box_c = []
         box_c_p = []
-        #img_w = image_size[1]
-        #img_h = image_size[0]
+        img_w = image_size[1]
+        img_h = image_size[0]
         for i,output in enumerate(outputs):
             boxes.append(output[..., 0:4])
             box_c.append(self.sigmoid(output[..., 4:5]))
             box_c_p.append(self.sigmoid(output[..., 5:]))
+
+            
             grid_h = boxes[i].shape[0]
             grid_w = boxes[i].shape[1]
             a = boxes[i].shape[2]
+
+
             anchor_w = self.anchors[i, :, 0]
             anchor_h = self.anchors[i, :, 1]
+
             tx = boxes[i][..., 0]
             ty = boxes[i][..., 1]
             tw = boxes[i][..., 2]
             th = boxes[i][..., 3]
+
             cx = np.indices((grid_h, grid_w, a))[1]
             cy = np.indices((grid_h, grid_w, a))[0]
-            bx = (self.sigmoid(tx) + cx) / grid_w
-            by = (self.sigmoid(ty) + cy) / grid_h
+            
             input_w = self.model.input.shape[1]
             input_h = self.model.input.shape[2]
 
 
+            bx = (self.sigmoid(tx) + cx) / grid_w
+            by = (self.sigmoid(ty) + cy) / grid_h
             bw = anchor_w * np.exp(tw) / input_w
             bh = anchor_h * np.exp(th) / input_h
 
+            
+            x1 = bx - bw / 2
+            x2 = x1 + bw
+            y1 = by - bh / 2
+            y2 = y1 + bh
 
-            x1 = (bx - (bw / 2)) * image_size[1]
-            y1 = (by - (bh / 2)) * image_size[0]
-            x2 = (bx + (bw / 2)) * image_size[1]
-            y2 = (by + (bh / 2)) * image_size[0]
-
-            # Append boxes
-            boxes[i][:, :, :, 0] = x1
-            boxes[i][:, :, :, 1] = y1
-            boxes[i][:, :, :, 2] = x2
-            boxes[i][:, :, :, 3] = y2
+            boxes[i][..., 0] = x1 * img_w
+            boxes[i][..., 1] = y1 * img_h
+            boxes[i][..., 2] = x2 * img_w
+            boxes[i][..., 3] = y2 * img_h
         return boxes, box_c, box_c_p
